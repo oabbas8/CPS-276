7840d3716722bbbd76f0984dcc1393a9e88a6e05
oabbas8 <123272185+oabbas8@users.noreply.github.com>
2023-02-05 23:00:49 -0500

    Update index.php

--- Assignments/assignment1/index.php
+++ Assignments/assignment1/index.php
@@ -1,12 +1,74 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p> hello Class </p>
-</body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Assignment1</title>
+  </head>
+  <body>
+  <div class="container">
+    <form>
+    <div class="mb-3">
+        <label for="fname" class="form-label">First Name</label>
+        <input type="text" class="form-control" id="fname">
+   
+    </div>
+   <div class="container">
+    <form>
+  <div class="mb-3">
+        <label for="lname" class="form-label">Last Name</label>
+        <input type="text" class="form-control" id="lname">
+  
+    </div>
+  <div class="container">
+    <form>
+  <div class="mb-3">
+        <label for="address" class="form-label">Address</label>
+        <input type="text" class="form-control" id="address">
+  
+    </div>
+  <div class="container">
+    <form>
+  <div class="mb-3">
+        <label for="city" class="form-label">City</label>
+        <input type="text" class="form-control" id="city">
+  
+    </div>
+  <fieldset abled>
+    <div class="mb-3">
+      <label for="State" class="form-label">State</label>
+      <select id="State" class="form-select">
+        <option>Michigan</option>
+        <option>Ohio</option>
+      </select>
+      <div class="container">
+    <form>
+  <div class="mb-3">
+        <label for="zipcode" class="form-label">Zip</label>
+        <input type="text" class="form-control" id="zipcode">
+       
+  <div class="form-check">
+  <input class="form-check-input" type="radio" name="Male" id="gender">
+  <label class="form-check-label" for="gender">
+    Male
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="radio" name="Female" id="gender">
+  <label class="form-check-label" for="gender">
+    Female
+  </label>
+</div>
+    </div>
+
+
+  <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+  </div>
+  </body>
 </html>
