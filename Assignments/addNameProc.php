51ff67c7324f7387d8cffe0087565e78b976f760
oabbas8 <123272185+oabbas8@users.noreply.github.com>
2023-02-26 22:03:43 -0500

    Add files via upload

---
+++ Assignments/addNameProc.php
@@ -0,0 +1,37 @@
+<?php
+class AddNamesProc{
+
+    public function addClearNames() {
+        if(isset($_POST['addNamesButton'])) {
+
+            if(isset($_POST['names'])) {
+                $nameArray = explode(" ", $_POST['names']);
+            }
+
+            $name = $nameArray[1]." ".$nameArray[0];
+
+            $newNameArray = explode("\n", $_POST['namelist']);
+
+            array_push($newNameArray, $name);
+
+            sort($newNameArray);
+
+            $output = implode("\n", $newNameArray);
+
+            return $output;
+        
+        }
+
+        if(isset($_POST['clearNamesButton'])) {
+            $_POST['namelist'] == null;
+        }
+
+        
+
+       
+
+    }
+
+
+}
+?>
