33a5ca0848bfd7dce9ef5871acc39fb9a9997e32
oabbas8 <123272185+oabbas8@users.noreply.github.com>
2023-03-12 01:30:08 -0500

    Update index.php

--- Assignments/index.php
+++ Assignments/index.php
@@ -1,7 +1,13 @@
 <?php
-require_once 'addNameProc.php';
-$addNames = new AddNamesProc();
-$output = $addNames->addClearNames();
+$msg="";
+
+if(isset($_POST['submit'])) {
+    require_once 'Directories.php';
+    $directory = new Directories();
+    $output = $directory->makeDir();
+}else {
+    $output = null;
+}
 
 ?>
 
@@ -12,35 +18,41 @@ $output = $addNames->addClearNames();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">    <title>Assignment 4</title>
+    <!-- Bootstrap CSS --> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">  
+    <title>Assignment 5</title>
 </head>
 <body>
+    <div id="wrapper" class="container">
+        <h1>File and Directory Assignment</h1>
+        <p>Enter a folder name and the contents of a file. Folder names should contain alpha numeric characters only.</p>
 
-    <main class="container">
+    <form method="POST" action="index.php">
 
-    <form method="post" action="index.php">
-    <h1>Add Names</h1>
+    <?php echo $output ?>
+    <?php echo $msg ?>
 
-    <div class="form-group">
-    <button type="submit" class="btn btn-primary" name="addNamesButton" id="addNamesButton">Add Names</button>
-    <button type="submit" class="btn btn-primary" name="clearNamesButton" id="clearNamesButton">Clear Names</button>
-    </div>  
 
     <div class="form-row">
           <div class="col">
-            <label for="names">Enter Names</label>
-            <input type="text" class="form-control" name= "names" id="names">
+            <label for="names">Folder Name</label>
+            <input type="text" class="form-control" name= "folderName" id="folderName">
           </div>
     </div>
 
     <div class="form-row">
-        <label for="namelist">List Of Names</label>
-        <textarea style="height: 500px;" class="form-control"
-        id="namelist" name="namelist"><?php echo $output?></textarea>
+        <label for="namelist">Contents</label>
+        <textarea style="height: 250px;" class="form-control"
+        id="contents" name="folderContents"></textarea>
+    </div>
+
+    <div class="form-row">
+        <input type="submit" class="btn btn-primary" name="submit" value="Submit">
     </div>
 
     </form>
 
+    </div>
+    
 </body>
 </html>
